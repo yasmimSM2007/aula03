4aa3eed806472ab5e34c3b28de41f22906f3e277
yasmimSM2007 <160530006+yasmimSM2007@users.noreply.github.com>
2024-10-21 18:37:18 -0300

    21.10

--- .history/src/pages/Home_20241021183524.jsx
+++ .history/src/pages/Home_20241021183524.jsx
@@ -24,7 +24,7 @@ receberListaProdutos();
                     <h2>{produto.title}</h2>
                     <p>{produto.description}</p>
                     <p>Preço:R${produto.price}</p>
-                    <img src={produto.image} alt={produto.title} width={100}/>
+                    <img src={produto.image alt={produto.title} width={100}/>
                 </li>
             ))}
         </ul>
